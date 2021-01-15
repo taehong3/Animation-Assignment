366735741547ed113fe2cffcea65828253113a32
taehong3 <73399332+taehong3@users.noreply.github.com>
2021-01-15 02:38:26 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 var gameEngine = new GameEngine();
 
 var ASSET_MANAGER = new AssetManager();
-
+ASSET_MANAGER.queueDownload("./megaman3.png")
 ASSET_MANAGER.downloadAll(function () {
 	var canvas = document.getElementById('gameWorld');
 	var ctx = canvas.getContext('2d');
 
 	gameEngine.init(ctx);
-
+	gameEngine.addEntity(new mega(gameEngine));
 	gameEngine.start();
 });
